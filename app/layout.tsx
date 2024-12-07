09ddb63c8c2cd1832ecacfb15fda857c85f0d8cd
Hugo Martínez Barros <hugo.martinez@digitalizat.com>
2024-12-07 13:34:44 +0100

    Añadir componente de análisis de Vercel en el diseño raíz

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
